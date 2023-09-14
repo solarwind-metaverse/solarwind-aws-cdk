796c81a7d19b93a57fccf21713766ebd5af85d19
Vladimir Halme <vhalme@gmail.com>
2023-09-14 16:49:56 +0300

    Initial commit

---
+++ test/solarwind-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SolarwindAwsCdk from '../lib/solarwind-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/solarwind-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SolarwindAwsCdk.SolarwindAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
